204d5d632af1b80fe234f20b9d1b563baf3ab4e3
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-02-11 23:46:43 -0300

    First Commit

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
+  templateUrl: `./home-page.component.html`,
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
